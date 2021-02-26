9638b17725980f25e824a2518cc2dbff9158eb8f
Hodogo <kolya.panow@yandex.ru>
2021-02-26 10:04:48 +0300

    add homepage

--- HouseApp/admin.py
+++ HouseApp/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import House,Flat,People
 
-admin.site.register(House)
+class FlatInline(admin.TabularInline):
+    model = Flat
+    extra = 1
+
+class HouseAdmin(admin.ModelAdmin):
+    inlines = [FlatInline]
+
+admin.site.register(House,HouseAdmin)
 admin.site.register(Flat)
 admin.site.register(People)
--- PassportOfficer/settings.py
+++ PassportOfficer/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'HouseApp',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
