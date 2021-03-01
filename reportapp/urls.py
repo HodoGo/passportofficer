c4d601d8925035c718af14aa8eca72af8c06c6e2
Hodogo <kolya.panow@yandex.ru>
2021-03-01 08:40:27 +0300

    add export xlsx

--- reportapp/urls.py
+++ reportapp/urls.py
@@ -7,4 +7,5 @@ app_name = 'reportapp'
 urlpatterns = [
     path('',views.people_list,name='people_list'),
     path('export_word/',views.export_word,name='export_word'),
+    path('export_excel/',views.export_excel,name='export_excel'),
 ]
--- reportapp/views.py
+++ reportapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from docxtpl import DocxTemplate
+from xlsxtpl.writerx import BookWriter
 from django.http import HttpResponse
 import os
 
@@ -25,7 +26,24 @@ def export_word(request):
   doc.save(response)
   return response
 
+def export_excel(request):
+  _PATH = os.path.abspath(os.path.dirname(__file__))
+  XLSX_TEMP = os.path.join(_PATH, 'static', 'xlsx','example.xlsx')
+  xlsx =  BookWriter(XLSX_TEMP)
+  xlsx.jinja_env.globals.update(dir=dir, getattr=getattr)
 
+  p = People.objects.select_related().order_by('flat').all()
+  
+  person_info = {'address': u'Test', 'name': u'rwe', 'fm': 178 }             
+  person_info['rows'] = p
+  person_info['tpl_idx'] = 0
+  payloads = [person_info]
 
+  xlsx.render_book(payloads=payloads)
+  response = HttpResponse(content_type='application/vnd.ms-excel')
+  response['Content-Disposition'] = 'attachment; filename=your_template_name.xlsx'
+  xlsx.save(response)
+
+  return response
 
 
